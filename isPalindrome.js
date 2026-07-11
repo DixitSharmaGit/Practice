dc65b9e98f63151d999353461b0041871a89c12e
DixitSharmaGit <162581075+DixitSharmaGit@users.noreply.github.com>
2026-07-11 22:24:40 +0530

    isPalindrome without inbuilt methods

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,9 +1,22 @@
+// // with inbuilt methods
+// function isPalindrome(str) {
+//   let revStre = str.split("").reverse().join("");
+//   if (str === revStre) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(isPalindrome(" om"));// false
+
+// without inbuilt methods
 function isPalindrome(str) {
-  let revStre = str.split("").reverse().join("");
-  if (str === revStre) {
+  const len = str.length;
+  for (i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
     return true;
-  } else {
-    return false;
   }
 }
-console.log(isPalindrome(" om"));// false
+console.log(isPalindrome("madam")); // true
