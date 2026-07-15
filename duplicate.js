0f9e617b50510b7ab481260cd0e4b465f78d596e
DixitSharmaGit <162581075+DixitSharmaGit@users.noreply.github.com>
2026-07-15 23:18:05 +0530

    remove duplicate from array

---
+++ duplicate.js
@@ -0,0 +1,5 @@
+function removeDuplicates(arr) {
+    const uniqueArr = [...new Set(arr)];
+    return uniqueArr;
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
