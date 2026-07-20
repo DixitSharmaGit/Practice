371c9a1df52e7a939f3f70ccbb2c191d929f937f
DixitSharmaGit <162581075+DixitSharmaGit@users.noreply.github.com>
2026-07-20 22:42:27 +0530

    remove duplicate without inbuilt function

--- duplicate.js
+++ duplicate.js
@@ -4,13 +4,32 @@
 // // }
 // // console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
 
+// function removeDuplicates(arr) {
+//     const uniqueArr = [];
+//     arr.forEach(element => {
+//         if (!uniqueArr.includes(element)) {
+//             uniqueArr.push(element);
+//         }
+//     });
+//     return uniqueArr;
+// }
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+
+// Without inbuilt methods
 function removeDuplicates(arr) {
     const uniqueArr = [];
-    arr.forEach(element => {
-        if (!uniqueArr.includes(element)) {
-            uniqueArr.push(element);
+    for (let i = 0; i < arr.length; i++) {
+        let isDuplicate = false;
+        for (let j = 0; j < uniqueArr.length; j++) {
+            if (arr[i] === uniqueArr[j]) {
+                isDuplicate = true;
+                break;
+            }
         }
-    });
+        if (!isDuplicate) {
+            uniqueArr.push(arr[i]);
+        }
+    }
     return uniqueArr;
 }
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
