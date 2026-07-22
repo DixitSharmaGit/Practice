ab3881c770ab3ddb4eee769cf643cfa384e0a4dc
DixitSharmaGit <162581075+DixitSharmaGit@users.noreply.github.com>
2026-07-22 22:50:32 +0530

    finding maximum count in a array with in built method

---
+++ findmaxCount.js
@@ -0,0 +1,15 @@
+//find the maximum count of consecutive 1's in an array
+function maxCount(arr) {
+    let maxCount = 0;
+    let currentCount = 0;
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i] === 1){
+            currentCount ++;
+        } else {
+            maxCount = Math.max(maxCount, currentCount);
+            currentCount = 0;
+        }
+    }
+    return maxCount = Math.max(maxCount, currentCount);
+}
+console.log(maxCount([1, 1, 0, 1, 1, 1])); // Output: 3
