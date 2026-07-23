136cc1c45f3f8ec177123beca023219792f41e73
DixitSharmaGit <162581075+DixitSharmaGit@users.noreply.github.com>
2026-07-23 23:28:47 +0530

    maxcount without in built method

--- findmaxCount.js
+++ findmaxCount.js
@@ -1,15 +1,33 @@
-//find the maximum count of consecutive 1's in an array
-function maxCount(arr) {
+// //find the maximum count of consecutive 1's in an array
+// function maxCount(arr) {
+//     let maxCount = 0;
+//     let currentCount = 0;
+//     for(i = 0; i < arr.length; i++) {
+//         if(arr[i] === 1){
+//             currentCount ++;
+//         } else {
+//             maxCount = Math.max(maxCount, currentCount);
+//             currentCount = 0;
+//         }
+//     }
+//     return maxCount = Math.max(maxCount, currentCount);
+// }
+// console.log(maxCount([1, 1, 0, 1, 1, 1])); // Output: 3
+
+// find the maximum count of consecutive 1's in an array without in built method
+function maxCount(arr){
     let maxCount = 0;
     let currentCount = 0;
     for(i = 0; i < arr.length; i++) {
-        if(arr[i] === 1){
+        if(arr[i] === 1) {
             currentCount ++;
         } else {
-            maxCount = Math.max(maxCount, currentCount);
+            if(currentCount > maxCount) {
+                maxCount = currentCount;
+            }
             currentCount = 0;
         }
     }
-    return maxCount = Math.max(maxCount, currentCount);
+    return maxCount = currentCount > maxCount ? currentCount : maxCount;
 }
 console.log(maxCount([1, 1, 0, 1, 1, 1])); // Output: 3
