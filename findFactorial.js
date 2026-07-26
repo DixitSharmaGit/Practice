9b356cd2f35e47707d03ab4aff6446f2d9afff5b
DixitSharmaGit <162581075+DixitSharmaGit@users.noreply.github.com>
2026-07-26 22:51:12 +0530

    factorial with out in built method

--- findFactorial.js
+++ findFactorial.js
@@ -1,8 +1,17 @@
-function factorial(num){
-    let result = 1;
-    for(let i = 1; i <= num; i++){
-        result *= i;//result=result*1
+// function factorial(num){
+//     let result = 1;
+//     for(let i = 1; i <= num; i++){
+//         result *= i;//result=result*1
+//     }
+//     return result
+// }
+// console.log(factorial(5))//120
+
+// without in built method
+function factorial(num) {
+    if (num === 0 || num === 1) {
+        return 1;
     }
-    return result
+    return num * factorial(num - 1);
 }
 console.log(factorial(5))//120
